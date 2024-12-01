3c1c8762973bcd8ad5f01725e49d281891e6bf94
Rico van Zelst <rvzelst7@gmail.com>
2024-12-01 14:57:09 +0100

    extract logger

--- src/index.js
+++ src/index.js
@@ -1,45 +1,8 @@
 const { Client, IntentsBitField } = require("discord.js");
 const chalk = require("chalk");
+const logger = require("./logger");
 require("dotenv").config();
 
-/**
- * Logger function to log messages to console
- *
- * @param {string} message
- * @param {string} [type="log"] - log type `(log, success, warn, error, info)`
- * @returns {void}
- */
-const logger = (message, type = "log") => {
-  const log = {
-    log: chalk.hex("#b8d2ff"),
-    success: chalk.green,
-    warn: chalk.yellow,
-    error: chalk.red,
-    info: chalk.blue,
-  };
-
-  if (!message) return console.log(log.error("[❌] No message provided."));
-  if (!type) return console.log(log.error("[❌] No log type provided."));
-
-  if (!log[type])
-    return console.log(log.error("[❌] Invalid log type provided."));
-
-  console.log(
-    log[type](
-      `${
-        type === "info"
-          ? "[🔎]"
-          : type === "warn"
-          ? "[⚠️]"
-          : type === "error"
-          ? "[❌]"
-          : type === "success"
-          ? "[✅]"
-          : ""
-      } ${message}`
-    )
-  );
-};
 
 const client = new Client({
   intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers],
---
+++ src/logger.js
@@ -0,0 +1,42 @@
+const chalk = require("chalk");
+
+/**
+ * Log messages to console
+ *
+ * @param {string} message
+ * @param {string} [type="log"] - log type `(log, success, warn, error, info)`
+ * @returns {void}
+ */
+const logger = (message, type = "log") => {
+  const log = {
+    log: chalk.hex("#b8d2ff"),
+    success: chalk.green,
+    warn: chalk.yellow,
+    error: chalk.red,
+    info: chalk.blue,
+  };
+
+  if (!message) return console.log(log.error("[❌] No message provided."));
+  if (!type) return console.log(log.error("[❌] No log type provided."));
+
+  if (!log[type])
+    return console.log(log.error("[❌] Invalid log type provided."));
+
+  console.log(
+    log[type](
+      `${
+        type === "info"
+          ? "[🔎]"
+          : type === "warn"
+          ? "[⚠️]"
+          : type === "error"
+          ? "[❌]"
+          : type === "success"
+          ? "[✅]"
+          : ""
+      } ${message}`
+    )
+  );
+};
+
+module.exports = logger;
