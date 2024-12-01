8b079953022a50e3e963e8fc127e3df00869ba6f
Rico van Zelst <rvzelst7@gmail.com>
2024-12-01 15:01:51 +0100

    feat: bot activity

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const { Client, IntentsBitField } = require("discord.js");
+const { Client, IntentsBitField, ActivityType } = require("discord.js");
 const chalk = require("chalk");
 const logger = require("./logger");
 require("dotenv").config();
-
 
 const client = new Client({
   intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers],
@@ -12,6 +11,10 @@ logger("Starting bot...", "info");
 client.login(process.env.BOT_TOKEN);
 
 client.on("ready", (bot) => {
+  bot.user.setActivity({
+    name: "(/) commands",
+    type: ActivityType.Watching,
+  });
   logger(`Logged in as ${bot.user.tag}`, "success");
   logger(
     `Detected ${bot.guilds.cache.size} servers and ${bot.users.cache.size} users`,
